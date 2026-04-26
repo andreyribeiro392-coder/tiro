e265b39c0385c2c643620b8f3280a489878ef65d
andreyribeiro392-coder <andreyribeiro392@gmail.com>
2026-04-25 22:26:30 -0300

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,23 +1,21 @@
-import playerImg from "./assets/player.png";
-
 export function startGame() {
+  const app = document.getElementById("app");
+
   const canvas = document.createElement("canvas");
   canvas.width = 800;
   canvas.height = 500;
-  document.body.appendChild(canvas);
+  app.appendChild(canvas);
 
   const ctx = canvas.getContext("2d");
 
+  // jogador
   const player = {
     x: 380,
     y: 420,
     w: 40,
     h: 40,
     speed: 5,
-    img: new Image(),
   };
-
-  player.img.src = playerImg;
 
   const keys = {};
 
@@ -32,10 +30,13 @@ export function startGame() {
   function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+    // fundo
     ctx.fillStyle = "#111";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.drawImage(player.img, player.x, player.y, player.w, player.h);
+    // jogador
+    ctx.fillStyle = "lime";
+    ctx.fillRect(player.x, player.y, player.w, player.h);
   }
 
   function loop() {
