48b1c4c1dbb755e974bcf8ca361c92a79ddd72ae
andreyribeiro392-coder <andreyribeiro392@gmail.com>
2026-04-25 22:19:18 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { startGame } from "./game.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App />
-);
+startGame();
